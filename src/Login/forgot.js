6e80355c39bbd6bb244cdfbb7f78c653f1619623
khom19 <khom2548@gmail.com>
2024-07-15 13:04:27 +0700

    reset password

--- src/Login/forgot.js
+++ src/Login/forgot.js
@@ -1,9 +1,27 @@
 import React , { Component } from 'react';
+import  './forgot.css'
+import { IoChevronBack } from "react-icons/io5";
+import { Link } from 'react-router-dom';
+import { MdEmail } from "react-icons/md";
 
 class Forgot extends Component {
     render() {
         return(
-            <div>forgot</div>
+            <div className='warpForgot'>
+                <div className='back'>
+                    <nav><Link to='/'><IoChevronBack /></Link></nav>
+                </div>
+                <div className='email'>Reset password</div>
+
+                <div className='emailbox'>
+                    <input type='text' placeholder='email'></input>
+                    <MdEmail className='mailIcon' />
+                </div>
+
+                <div className='submit'>
+                    <button type='submit'>Submit</button>
+                </div>
+            </div>
         )
     }
 }
--- src/Login/signUp.js
+++ src/Login/signUp.js
@@ -3,7 +3,7 @@ import React , { Component } from 'react';
 class SignUp extends Component {
     render() {
         return(
-            <div>SignUp</div>
+            <div className='warpSingUp'>SignUp</div>
         )
     }
 }
