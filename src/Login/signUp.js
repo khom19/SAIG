6ee5713fb5ac0702ddfcb7badd3d6ced85be08cb
khom19 <khom2548@gmail.com>
2024-07-18 17:51:12 +0700

    try to write function

--- src/Login/signUp.js
+++ src/Login/signUp.js
@@ -1,14 +1,20 @@
 import React , { Component } from 'react';
 import './signUp.css'
-import { Link } from 'react-router-dom';
+import { Form, Link } from 'react-router-dom';
 import { IoChevronBack } from "react-icons/io5";
 import { MdEmail } from "react-icons/md";
 import { FaUser , FaLock } from "react-icons/fa";
+import './onclick'
+
+const handleSubmit = (e) => {
+    
+}
 
 class SignUp extends Component {
     render() {
         return(
-        <section className='signupbackground'>
+    <section className='signupbackground'>
+        <form onSubmit={handleSubmit}>
             <div className='warpSingUp'>
                 <div className='back'>
                     <nav><Link to='/'><IoChevronBack /></Link></nav>
@@ -32,10 +38,11 @@ class SignUp extends Component {
                     <FaLock className='icon'/>
                 </div>
                 <div className='submit'>
-                    <button type='submit'>Sign up</button>
+                    <button type='submit' >Sign up</button>
                 </div>
             </div>
-        </section>
+        </form>
+    </section>
         )
     }
 }
